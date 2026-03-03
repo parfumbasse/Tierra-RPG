9b8feab83eac1178bd64326521025c2a23f73c2b
parfumbasse <parfumbasse@gmail.com>
2026-03-03 07:41:08 -0300

    ggg2

---
+++ creditos.asm
@@ -0,0 +1,17 @@
+Topdown 2D RPG Movement is a Scene plug-in for GB Studio 4.x created by 8bitKittyKat. https://8bitkittykat.itch.io/topdown-2d-rpg-movement
+
+Personajes, RPG Tsunkuru GBC 1 y 2 e itchio
+https://callmedj169.itch.io/gb-rm-characters
+https://www.spriters-resource.com/game_boy_gbc/rpgtsukurugb/
+https://www.spriters-resource.com/game_boy_gbc/rpgtsukurugb2/
+
+escenarios y entorno, Final Fantasy III Famicom y Polished Crystal.
+https://www.videogamesprites.net/FinalFantasy3/
+https://www.spriters-resource.com/nes/finalfantasy3jpn/
+https://www.spriters-resource.com/game_boy_gbc/pokemoncrystal/
+
+fuentes, https://github.com/mxashlynn/gb-studio-fonts/tree/main
+https://edo999.itch.io/cookie-pixel-font
+
+battle system
+https://www.youtube.com/watch?v=D0iyKneUq18&list=WL&index=3
